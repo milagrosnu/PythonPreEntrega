51d6844a7eca022d5969e1bf098a7df2232b930f
Milagros Nuñez <milagros.nuag@gmail.com>
2022-11-04 23:30:43 -0300

    commit

--- appcoderhouse/admin.py
+++ appcoderhouse/admin.py
@@ -6,5 +6,3 @@ from appcoderhouse.models import Alumno, Profesor, Curso
 admin.site.register(Alumno)
 admin.site.register(Profesor)
 admin.site.register(Curso)
-
-#Super user admin, perezxpamela@gmail.com, Marzo2022
